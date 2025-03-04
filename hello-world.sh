e19aa9d48492d1b5b6b197500d4eaa142bb481f4
Erik Fasterius <erik.fasterius@outlook.com>
2025-03-04 09:26:03 +0100

    Add a comment to main

--- hello-world.sh
+++ hello-world.sh
@@ -4,13 +4,15 @@
 # This is the best implementation of this, ever.
 # This is a new comment.
 
+# Hello function
 hello() {
     echo "Hello, world!"
 }
 
- goodbye() {
-     echo "Goodbye, world!"
- }
+# Goodbye function
+goodbye() {
+    echo "Goodbye, world!"
+}
 
 # My main function
 main() {
